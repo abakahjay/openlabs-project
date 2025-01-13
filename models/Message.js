5c42c4247e4e5fd2af6148d57f77f60f09aff52f
abakahjay <joshuaabakahhh@gmail.com>
2025-01-13 04:40:06 +0000

    d

--- models/Message.js
+++ models/Message.js
@@ -1,11 +1,29 @@
 const mongoose = require('mongoose');
 
 const MessageSchema = new mongoose.Schema({
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    message: { type: String, required: true },
-    read: { type: Boolean, default: false },
-    timestamp: { type: Date, default: Date.now },
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Please Provide the Sender UserId']
+    },
+    recipient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Please Provide the Receiver UserId']
+    },
+    message: {
+        type: String,
+        required: [true, 'Please Provide the Message']
+    },
+    read: {
+        type: Boolean,
+        default: false
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    },
 });
 
-module.exports = mongoose.model('Message', MessageSchema);
+const Message = mongoose.model('Message', MessageSchema);
+module.exports = Message;
